52f615e735b940a081db6cb31a41ba913dd1ca41
Ray Wu <pickupfaith@gmail.com>
2023-03-30 21:16:42 +0800

    finish02

--- components/FooTer.vue
+++ components/FooTer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="footer bg-white mt-5 text-center text-dark font-weight-bolder border-top border-dark py-2 fixed-bottom">
-        Power BY Ray 2022.09</div>
+        Power BY Ray 2023.03</div>
 </template>
 
 <script setup>
